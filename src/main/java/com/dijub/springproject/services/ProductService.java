5808a8e4a982c8b526e6b1208e33892e4d2026a1
diego-insaurralde <diego_insaurralde@live.com>
2024-07-03 17:44:36 -0300

    Add Entity/Resource/Service of Product

---
+++ src/main/java/com/dijub/springproject/services/ProductService.java
@@ -0,0 +1,32 @@
+package com.dijub.springproject.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dijub.springproject.entities.Product;
+import com.dijub.springproject.repositories.ProductRepository;
+
+/**
+ * ProductService
+ */
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> findAll() {
+        return repository.findAll();
+    }
+
+    public Product findById(Long id) {
+        Optional<Product> obj = repository.findById(id);
+        return obj.get();
+
+    }
+
+}
