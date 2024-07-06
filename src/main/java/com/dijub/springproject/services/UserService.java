f5b5d20691d7a1d9b5d07cc295158b4ed8e01c6d
diego-insaurralde <diego_insaurralde@live.com>
2024-07-06 17:35:40 -0300

    add 404 to update endpoint

--- src/main/java/com/dijub/springproject/services/UserService.java
+++ src/main/java/com/dijub/springproject/services/UserService.java
@@ -5,14 +5,14 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PutMapping;
 
 import com.dijub.springproject.entities.User;
 import com.dijub.springproject.repositories.UserRepository;
 import com.dijub.springproject.services.exceptions.DatabaseException;
 import com.dijub.springproject.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 /**
  * UserService
@@ -51,9 +51,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
