de423021d035feb74d0c1238b3cdee50692a36e2
diego-insaurralde <diego_insaurralde@live.com>
2024-07-06 13:01:08 -0300

    add 404 to delete nonexistent id and 400 to delete integrity constraint violation

---
+++ src/main/java/com/dijub/springproject/services/exceptions/DatabaseException.java
@@ -0,0 +1,12 @@
+package com.dijub.springproject.services.exceptions;
+
+/**
+ * DatabaseException
+ */
+public class DatabaseException extends RuntimeException {
+
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+
+}
